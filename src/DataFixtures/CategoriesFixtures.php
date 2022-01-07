89b8d993004c21fdfcec90505e87db259c01812d
Jonathan Plastivène <jonathan.plastivene@outlook.com>
2022-01-07 20:11:43 +0100

    commit du 7/01/2022 ajout du fichiers datafixtures

--- src/DataFixtures/CategoriesFixtures.php
+++ src/DataFixtures/CategoriesFixtures.php
@@ -1,17 +1,37 @@
 <?php
 
 namespace App\DataFixtures;
-
+use App\Entity\Categories;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 
 class CategoriesFixtures extends Fixture
 {
+
+    //Je crée un tableau pour mes data fictives
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $categories = [
+            1 => [
+                'name' => 'Tourisme',
+            ],
+            2 => [
+                'name' => 'sportive',
+            ],
+            3 => [
+                'name' => 'utilitaires',
+            ],
 
-        $manager->flush();
+        ];
+        // je parcours mon tableau avec ma boucle foreach
+        foreach ($categories as $key => $value)
+        {
+            $categorie = new Categories();
+            $categorie->setName($value['name']);
+            $manager->persist($categorie);
+        }
+
+            $manager->flush();
+
     }
 }
