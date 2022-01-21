4ec1f064076bcace4d44fb42755635b1ee76ef9e
Jonathan Plastivène <jonathan.plastivene@outlook.com>
2022-01-21 18:52:45 +0100

    espace client sidebar + recup des données ok

--- src/Controller/UsersController.php
+++ src/Controller/UsersController.php
@@ -1,20 +1,79 @@
 <?php
 
 namespace App\Controller;
-
+use App\Form\UpdateUserType;
+use App\Services\BookingService;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+
 
 class UsersController extends AbstractController
 {
+    /**
+     * bs
+     *
+     * @var BookingService
+     */
+    private $bs;
+    
+    /**
+     * em
+     *
+     * @var EntityManager
+     */
+    private $em;
+
+    public function __construct(BookingService $bs, EntityManagerInterface $em)
+    {
+        $this->bs = $bs;
+        $this->em = $em;
+    }
+
     #[Route('/users', name: 'users')]
     public function index(): Response
     {
-        return $this->render('users/users.html.twig', [
-            'controller_name' => 'UsersController',
+        return $this->render('users/index.html.twig', [
+            'booking' => $this->bs->bookingsList(),
         ]);
     }
+
+    #[Route('/users/update', name: 'users_update')]    
+    /**
+     * Method pour que l'utilisateur puisse changer son email 
+     *
+     * @param Request $request [explicite description]
+     *
+     * @return Response
+     */
+    public function updateUser(Request $request): Response
+    {
+        $user = $this->getUser();
+        $formUser = $this->createForm(UpdateUserType::class, $user);
+
+        $formUser->handleRequest($request);
+
+        if ($formUser->isSubmitted() && $formUser->isValid()) 
+        {
+            $this->em->persist($user);
+            $this->em->flush();
+
+            $this->addFlash('message', 'Profil mis a jour');
+            return $this->redirectToroute('users');
+        }
+        return $this->render('users/updateuser.html.twig', [
+            'formUser' => $formUser->createView(),
+        ]);
+    }
+
+
+
+
+
+
+
 
     // #[Route('/users', name: 'users')]
     // public function index(): Response
---
+++ src/Form/UpdateUserType.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Users;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+
+
+class UpdateUserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('firstname', TextType::class)
+            ->add('lastname', TextType::class)
+            ->add('email', EmailType::class, [
+                'constraints' => [
+                    new NotBlank([
+                        'message' => 'Email requis'
+                    ])
+                    ], 'attr' => [
+                        'class' => 'form-control',
+                    ],
+                ])
+            ->add('Valider', SubmitType::class)
+            
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Users::class,
+        ]);
+    }
+}
